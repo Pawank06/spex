da33eb08263f5f76aedf9e2a8fb6da153fe40852
pawank06 <pawan06kumar2003@gmail.com>
2026-01-16 23:51:52 +0530

    feat: init reassemble test

--- spex-core/src/reassemble.rs
+++ spex-core/src/reassemble.rs
@@ -2,6 +2,7 @@ use crate::chunk::Chunk;
 
 pub fn reassemble_chunks(mut chunks: Vec<Chunk>) -> Vec<u8> {
     chunks.sort_by_key(|item| item.index);
+    
     let mut result = Vec::new();
     
     for chunk in chunks {
@@ -10,4 +11,18 @@ pub fn reassemble_chunks(mut chunks: Vec<Chunk>) -> Vec<u8> {
     
     result
     
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn join_data_from_chunks() {
+        let mut chunks: Vec<Chunk> = vec![{1; 1}];
+        
+        let data = reassemble_chunks(chunks);
+        
+        
+    }
 }
