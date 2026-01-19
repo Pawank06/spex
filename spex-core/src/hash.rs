07b719920e0fb6fbc8eb963911f90946cdea07c3
pawank06 <pawan06kumar2003@gmail.com>
2026-01-19 18:57:45 +0530

    feat: implement chunk hashing

--- spex-core/src/hash.rs
+++ spex-core/src/hash.rs
@@ -1,5 +1,7 @@
 use sha2::{Digest, Sha256};
 
+use crate::chunk::Chunk;
+
 pub type Hash = [u8; 32];
 
 pub fn hash_bytes(data: &[u8]) -> Hash {
@@ -8,9 +10,14 @@ pub fn hash_bytes(data: &[u8]) -> Hash {
     hasher.finalize().into()
 }
 
+pub fn hash_chunk(chunk: &Chunk) -> Hash {
+    hash_bytes(&chunk.data)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::chunk::Chunk;
     
     #[test]
     fn same_bytes_produce_same_hash() {
@@ -27,4 +34,41 @@ mod tests {
         
         assert_ne!(h1, h2);
     }
+    
+    #[test]
+    fn same_chunk_data_same_hash() {
+        let c1 = Chunk {
+            index: 0,
+            data: b"hello".to_vec(),
+        };
+        
+        let c2 = Chunk {
+            index: 1,
+            data: b"hello".to_vec()
+        };
+        
+        let hash1 = hash_chunk(&c1);
+        let hash2 = hash_chunk(&c2);
+        
+        assert_eq!(hash1, hash2);
+    }
+    
+    #[test]
+    fn different_chunk_data_different_hash() {
+        let c1 = Chunk {
+            index: 0,
+            data: b"hello".to_vec(),
+        };
+        
+        let c2 = Chunk {
+            index: 1,
+            data: b"world".to_vec()
+        };
+        
+        let hash1 = hash_chunk(&c1);
+        let hash2 = hash_chunk(&c2);
+        
+        assert_ne!(hash1, hash2);
+    }
+    
 }
