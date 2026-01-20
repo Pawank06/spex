5a5566342926c09d71528212d8691076dc343d06
pawank06 <pawan06kumar2003@gmail.com>
2026-01-20 22:59:06 +0530

    feat: add task_a concurrent async task

--- spex-net/src/main.rs
+++ spex-net/src/main.rs
@@ -1,15 +1,8 @@
 use tokio::time::{sleep, Duration};
 
-#[tokio::main]
-async fn main() {
-    tokio::spawn(async {
-        loop {
-            println!("tick");
-            sleep(Duration::from_secs(1)).await;
-        }
-    });
-
-    let msg = spex_net::async_wait_and_hello_world().await;
-    println!("{msg}");
-    println!("done");
+async fn task_a() {
+    for i in 1..=3 {
+        println!("A: Sleep {i}");
+        sleep(Duration::from_secs(1)).await;
+    }
 }
