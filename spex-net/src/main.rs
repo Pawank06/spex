2b924e88e7e456cb95dd8e3a8b1b0f27a9dd643e
pawank06 <pawan06kumar2003@gmail.com>
2026-01-20 23:05:04 +0530

    feat: add task_b concurrent async task

--- spex-net/src/main.rs
+++ spex-net/src/main.rs
@@ -6,3 +6,10 @@ async fn task_a() {
         sleep(Duration::from_secs(1)).await;
     }
 }
+
+async fn task_b() {
+    for i in  1..3{
+        println!("A: sleep {i}");
+        sleep(Duration::from_secs(1)).await;
+    }
+}
