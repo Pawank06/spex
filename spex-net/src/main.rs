7fd623343cd79c51d28ff970ed982cc829cda190
pawank06 <pawan06kumar2003@gmail.com>
2026-01-21 23:57:08 +0530

    feat: create a receiver peer task

--- spex-net/src/main.rs
+++ spex-net/src/main.rs
@@ -1,3 +1,5 @@
+use std::any::Any;
+
 use tokio::sync::mpsc;
 use tokio::time::{sleep, Duration};
 
@@ -17,8 +19,40 @@ async fn sender(mut tx: mpsc::Sender<NetMessage>) {
     
     for chunk in chunks {
         println!("sender: sending chunk {}", chunk.index);
-        tx.send(Duration::from_millis(500)).await;
+        tx.send(NetMessage::Chunk(chunk)).await.unwrap();
+        sleep(Duration::from_millis(500)).await;
     }
     
     println!("sender: done")
+}
+
+async fn receiver(mut rx: mpsc::Receiver<NetMessage>) {
+    while let Some(msg) = rx.recv().await {
+        match msg {
+            NetMessage::FileMeta(meta) => {
+                println!("reciever: got metadata ({} chunks, {} bytes)", meta.total_chunks, meta.file_size);
+            }
+            
+            NetMessage::Chunk(chunk) => {
+                println!("receiver: got chunk {} ({} bytes)",
+                chunk.index,
+                chunk.data.len());
+            }
+            
+            NetMessage::RequestChunk { index } => {
+                println!("reciver: requested chunk {index}");
+            }
+        }
+    }
+    println!("reciver: channel closed");
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = mpsc::channel(10);
+    
+    let sender_task = tokio::spawn(sender(tx));
+    let receiver_task = tokio::spawn(receiver(rx));
+    
+    let _ = tokio::join!(sender_task, receiver_task);
 }
