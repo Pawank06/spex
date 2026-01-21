f527e8e3697ca2c2f8aa2b388095bafeb7c4f147
pawank06 <pawan06kumar2003@gmail.com>
2026-01-21 22:23:21 +0530

    async channels

--- spex-net/src/main.rs
+++ spex-net/src/main.rs
@@ -1,25 +1,20 @@
+use tokio::sync::mpsc;
 use tokio::time::{sleep, Duration};
-
-async fn task_a() {
-    for i in 1..=3 {
-        println!("A: Sleep {i}");
-        sleep(Duration::from_secs(1)).await;
-    }
-}
-
-async fn task_b() {
-    for i in  1..3{
-        println!("A: sleep {i}");
-        sleep(Duration::from_secs(1)).await;
-    }
-}
 
 #[tokio::main]
 async fn main() {
-    let a = tokio::spawn(task_a());
-    let b = tokio::spawn(task_b());
+    let (tx, mut rx) = mpsc::channel(10);
     
-    let _ = tokio::join!(a, b);
+    tokio::spawn(async move {
+        for i in 1..=5 {
+            tx.send(format!("message {i}")).await.unwrap();
+            sleep(Duration::from_secs(1)).await;
+        }
+    });
     
-    println!("Both tasks completed");
+    while let Some(msg) = rx.recv().await {
+        println!("recived: {msg}");
+    }
+    
+    println!("channel closed")
 }
