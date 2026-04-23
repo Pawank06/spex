6f7dc6595ea4960557a7764f2ac7cc1863e47bf2
pawank06 <pawan06kumar2003@gmail.com>
2026-04-23 10:08:00 +0800

    drop dead peer module

--- spex-net/src/peer.rs
+++
@@ -1,9 +0,0 @@
-use spex_core::metadata::FileMeta;
-use spex_core::chunk::Chunk;
-
-#[derive(Debug)]
-pub enum NetMessage {
-    FileMeta(FileMeta),
-    Chunk(Chunk),
-    RequestChunk {index: u64}
-}
