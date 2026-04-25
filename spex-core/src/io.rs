a8e6ca6aee6e4e506b8d4a7d0a639b069d0142c2
pawank06 <pawan06kumar2003@gmail.com>
2026-04-25 11:46:00 +0800

    add file writer

--- spex-core/src/io.rs
+++ spex-core/src/io.rs
@@ -5,3 +5,7 @@ use std::path::Path;
 pub fn read_file(path: &Path) -> io::Result<Vec<u8>> {
     fs::read(path)
 }
+
+pub fn write_file(path: &Path, data: &[u8]) -> io::Result<()> {
+    fs::write(path, data)
+}
