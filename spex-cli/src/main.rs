fc407ad6e8d2d1f9b504216b85c0a3d443f860b4
pawank06 <pawan06kumar2003@gmail.com>
2026-04-28 17:38:00 +0800

    init tracing subscriber

--- spex-cli/src/main.rs
+++ spex-cli/src/main.rs
@@ -9,6 +9,13 @@ use spex_net::{receiver, sender};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
+
     let args = Cli::parse();
 
     match args.cmd {
