33a57883d146de0771deca423ccdbac3fcd4c41d
pawank06 <pawan06kumar2003@gmail.com>
2026-05-01 15:51:00 +0800

    test protocol roundtrips

--- spex-net/src/protocol.rs
+++ spex-net/src/protocol.rs
@@ -8,3 +8,36 @@ pub enum NetMessage {
     Chunk(Chunk),
     RequestChunk { index: u64 }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn request_chunk_roundtrips() {
+        let msg = NetMessage::RequestChunk { index: 7 };
+        let bytes = bincode::serialize(&msg).unwrap();
+        let decoded: NetMessage = bincode::deserialize(&bytes).unwrap();
+
+        match decoded {
+            NetMessage::RequestChunk { index } => assert_eq!(index, 7),
+            _ => panic!("wrong variant"),
+        }
+    }
+
+    #[test]
+    fn chunk_roundtrips() {
+        let chunk = Chunk { index: 3, data: b"abc".to_vec() };
+        let msg = NetMessage::Chunk(chunk.clone());
+        let bytes = bincode::serialize(&msg).unwrap();
+        let decoded: NetMessage = bincode::deserialize(&bytes).unwrap();
+
+        match decoded {
+            NetMessage::Chunk(c) => {
+                assert_eq!(c.index, chunk.index);
+                assert_eq!(c.data, chunk.data);
+            }
+            _ => panic!("wrong variant"),
+        }
+    }
+}
